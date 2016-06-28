a31685c11a5deef927e66437608b4f758a1796dd
esailers <eric@ericsailers.com>
2016-06-28 13:47:12 -0700

    feat: Use a geocoder object to print latitude and longitude for an address string

--- OnTheMap/PostLocationViewController.swift
+++ OnTheMap/PostLocationViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import MapKit
 
-class PostLocationViewController: UIViewController {
+class PostLocationViewController: UIViewController, UITextFieldDelegate {
     
     // MARK: - Properties
     
@@ -18,10 +18,14 @@ class PostLocationViewController: UIViewController {
     @IBOutlet weak var toolbar: UIToolbar!
     @IBOutlet weak var mapView: MKMapView!
     
+    var geocoder: CLGeocoder? = nil
+    
     // MARK: - UIViewController lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        textField.delegate = self
 
         navigationItem.title = "Where are you studying?"
     }
@@ -30,11 +34,41 @@ class PostLocationViewController: UIViewController {
     
     @IBAction func findTapped(sender: UIButton) {
         textField.resignFirstResponder()
+        
+        if geocoder == nil {
+            geocoder = CLGeocoder()
+        }
+        
+        if let text = textField.text {
+            geocoder?.geocodeAddressString(text, completionHandler: {
+                (placemarks, error) in
+                
+                guard error == nil else {
+                    print("There was an error")
+                    return
+                }
+                
+                let placemark = placemarks?.first
+                let latitude = placemark?.location?.coordinate.latitude
+                let longitude = placemark?.location?.coordinate.longitude
+                if let latitude = latitude, longitude = longitude {
+                    let locationString = "\(latitude), \(longitude)"
+                    print(locationString)
+                }
+            })
+        }
     }
     
     @IBAction func cancelTapped(sender: UIBarButtonItem) {
         textField.resignFirstResponder()
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // MARK: - UITextFieldDelegate
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 
 }
